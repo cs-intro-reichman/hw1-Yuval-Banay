317c11dc81568eedbc26fb97f83c0e518ced7f3b
Yuval Banay <yuvalbanay@Yuvals-MacBook-Air.local>
2024-11-09 11:55:41 +0200

    completed

--- Ascend.java
+++ Ascend.java
@@ -2,6 +2,19 @@
 // and prints them in increasing order.
 public class Ascend {
 	public static void main(String[] args) {
-		// Replace this comment with your code
+	int x = Integer.parseInt(args[0]);
+	int a = (int) (x * Math.random());
+	int b = (int) (x * Math.random());
+	int c = (int) (x * Math.random());
+
+	// finding min and max
+	int min = Math.min(a, Math.min(b, c));
+	int max = Math.max(a ,Math.max(b, c));
+	// Finding the middle number
+	int sum = a + b + c;
+	int mid = sum - min - max;
+
+	System.out.println(a + " " + b + " " + c);
+	System.out.println(min + " " + mid + " " + max);
 	}
 }
