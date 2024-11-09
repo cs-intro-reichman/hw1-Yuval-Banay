ceeb67c1ba03bf5be630367df22465129820e3b5
Yuval Banay <yuvalbanay@Yuvals-MacBook-Air.local>
2024-11-09 14:25:14 +0200

    completed

--- FVCalc.java
+++ FVCalc.java
@@ -4,8 +4,8 @@ public class FVCalc {
 		int currentValue = Integer.parseInt(args[0]);
 		double interest = Double.parseDouble(args[1]);
 		int time = Integer.parseInt(args[2]);
-		double interestgrowth = interest / 100;
-		double futureValue = currentValue * Math.pow((1.0 + interestgrowth), time);
+		double interestGrowth = interest / 100;
+		double futureValue = currentValue * Math.pow((1.0 + interestGrowth), time);
 		System.out.println("After " + time + " years, a $" + currentValue + 
 							" saved at " + interest + "% will yield $" + (int) futureValue);
 	}
